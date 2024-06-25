a169e29617844f003fbd6ce319c5e291a9b7eba2
jason.kuczek@eidg-ltd.com <hB^L2NMWkEhu>
2024-06-25 11:51:54 -0600

    Add setter functions and unint8

--- contracts/EthUint.sol
+++ contracts/EthUint.sol
@@ -4,6 +4,18 @@ pragma solidity 0.8.26;
 
 contract MyUint {
 
-    uint public myUint;
-    
+    //variable declaration
+    uint256 public myUint = 83756;
+
+    uint8 public myUint8;
+
+    //setter function
+    function setMyUint(uint _myUint) public {
+        myUint = _myUint;
+    }
+
+    function setMyUint8(uint8 _myUint8) public {
+        myUint8 = _myUint8;
+    }
+
 }
