6ec80a1600731d07b11d5f23affaac419fc99065
jason.kuczek@eidg-ltd.com <hB^L2NMWkEhu>
2024-06-26 18:21:28 -0600

    Add J26P Contract

---
+++ contracts/EthJ26P.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract EthJ26P {
+
+    bool public myBoolean = true;
+
+    function setMyBoolean(bool _myBoolean) public {
+    myBoolean = _myBoolean;
+    }
+}
