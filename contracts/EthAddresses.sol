b72628954fd1dee2caf20b30edd4d49c77bdb85b
jason.kuczek@eidg-ltd.com <hB^L2NMWkEhu>
2024-06-27 14:59:24 -0600

    Add address setter function

--- contracts/EthAddresses.sol
+++ contracts/EthAddresses.sol
@@ -5,4 +5,11 @@ contract EthAddresses {
 
     address public myAddress;
 
+    //function to set address
+    function setMyAddress (address _myAddress) public {
+
+        myAddress = _myAddress;
+        
+    }
+
 }
